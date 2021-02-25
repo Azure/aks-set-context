703b78abd76a8dcdd6b4047f0247ae7902dad0f7
Deepak Sattiraju <ds-ms@github.com>
2021-02-25 14:03:40 +0530

    Code changes

--- src/login.ts
+++ src/login.ts
@@ -1,50 +1,8 @@
 import * as core from '@actions/core';
 import * as path from 'path';
 import * as fs from 'fs';
-import { WebRequest, WebRequestOptions, WebResponse, sendRequest } from "./client";
-import * as querystring from 'querystring';
-
-function getAzureAccessToken(servicePrincipalId, servicePrincipalKey, tenantId, authorityUrl, managementEndpointUrl : string): Promise<string> {
-
-    if (!servicePrincipalId || !servicePrincipalKey || !tenantId || !authorityUrl) {
-        throw new Error("Not all values are present in the creds object. Ensure appId, password and tenant are supplied");
-    }
-    return new Promise<string>((resolve, reject) => {
-        let webRequest = new WebRequest();
-        webRequest.method = "POST";
-        webRequest.uri = `${authorityUrl}/${tenantId}/oauth2/token/`;
-        webRequest.body = querystring.stringify({
-            resource: managementEndpointUrl,
-            client_id: servicePrincipalId,
-            grant_type: "client_credentials",
-            client_secret: servicePrincipalKey
-        });
-        webRequest.headers = {
-            "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
-        };
-
-        let webRequestOptions: WebRequestOptions = {
-            retriableStatusCodes: [400, 408, 409, 500, 502, 503, 504],
-        };
-
-        sendRequest(webRequest, webRequestOptions).then(
-            (response: WebResponse) => {
-                if (response.statusCode == 200) {
-                    resolve(response.body.access_token);
-                }
-                else if ([400, 401, 403].indexOf(response.statusCode) != -1) {
-                    reject('ExpiredServicePrincipal');
-                }
-                else {
-                    reject('CouldNotFetchAccessTokenforAzureStatusCode');
-                }
-            },
-            (error) => {
-                reject(error)
-            }
-        );
-    });
-}
+import { WebRequest, WebRequestOptions, WebResponse, sendRequest } from "@azure-actions/utilities/lib/http";
+import { getAzureAccessToken } from '@azure-actions/auth';
 
 function getAKSKubeconfig(azureSessionToken: string, subscriptionId: string, managementEndpointUrl: string): Promise<string> {
     let resourceGroupName = core.getInput('resource-group', { required: true });
@@ -78,13 +36,9 @@ async function getKubeconfig(): Promise<string> {
         throw new Error('Credentials object is not a valid JSON');
     }
 
-    let servicePrincipalId = credsObject["clientId"];
-    let servicePrincipalKey = credsObject["clientSecret"];
-    let tenantId = credsObject["tenantId"];
-    let authorityUrl = credsObject["activeDirectoryEndpointUrl"] || "https://login.microsoftonline.com";
     let managementEndpointUrl = credsObject["resourceManagerEndpointUrl"] || "https://management.azure.com/";
     let subscriptionId = credsObject["subscriptionId"];
-    let azureSessionToken = await getAzureAccessToken(servicePrincipalId, servicePrincipalKey, tenantId, authorityUrl, managementEndpointUrl);
+    let azureSessionToken = await getAzureAccessToken(creds);
     let kubeconfig = await getAKSKubeconfig(azureSessionToken, subscriptionId, managementEndpointUrl);
     return kubeconfig;
 }
