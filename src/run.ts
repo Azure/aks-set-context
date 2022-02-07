696975612c4f942b7549c8c8aeb74e8e8b1792e9
Oliver King <kingoliver@microsoft.com>
2022-02-07 17:25:44 -0500

    refactor project to rely on azure/login and az

--- src/run.ts
+++ src/run.ts
@@ -1,103 +1,49 @@
 import * as core from "@actions/core";
+import * as io from "@actions/io";
+import * as exec from "@actions/exec";
 import * as path from "path";
 import * as fs from "fs";
-import {
-  WebRequest,
-  WebResponse,
-  sendRequest,
-} from "@azure-actions/utilities/lib/http";
-import { getAzureAccessToken } from "@azure-actions/auth";
+
+const AZ_TOOL_NAME = "az";
 
 export async function run() {
   // get inputs
-  const creds = core.getInput("creds", { required: true });
-  let credsObject: { [key: string]: string };
-  try {
-    credsObject = JSON.parse(creds);
-  } catch (ex) {
-    throw new Error("Credentials object is not a valid JSON: " + ex);
-  }
-  const subscriptionId =
-    core.getInput("subscription-id") || credsObject["subscriptionId"];
   const resourceGroupName = core.getInput("resource-group", { required: true });
   const clusterName = core.getInput("cluster-name", { required: true });
 
-  // get kubeconfig
-  core.debug("Getting kubeconfig");
-  const kubeconfig = await getKubeconfig(
-    credsObject,
-    subscriptionId,
-    resourceGroupName,
-    clusterName
-  );
+  // check az tools
+  const azPath = await io.which(AZ_TOOL_NAME, false);
+  if (!azPath)
+    throw Error(
+      "Az cli tools not installed. You must install them before running this action."
+    );
 
-  // create file
+  // get kubeconfig
   const runnerTempDirectory = process.env["RUNNER_TEMP"]; // use process.env until the core libs are updated
   const kubeconfigPath = path.join(
     runnerTempDirectory,
     `kubeconfig_${Date.now()}`
   );
   core.debug(`Writing kubeconfig to ${kubeconfigPath}`);
-  fs.writeFileSync(kubeconfigPath, kubeconfig);
+  await exec.exec(
+    AZ_TOOL_NAME,
+    [
+      "aks",
+      "get-credentials",
+      "--resource-group",
+      resourceGroupName,
+      "--name",
+      clusterName,
+      "-f",
+      kubeconfigPath,
+    ],
+    { failOnStdErr: true }
+  );
   fs.chmodSync(kubeconfigPath, "600");
 
-  // export
+  // export variable
   core.exportVariable("KUBECONFIG", kubeconfigPath);
-  core.debug("KUBECONFIG environment variable is set");
-}
-
-export async function getKubeconfig(
-  creds: {
-    [key: string]: string;
-  },
-  subscriptionId: string,
-  resourceGroupName: string,
-  clusterName: string
-): Promise<string> {
-  const managementEndpointUrl =
-    creds["resourceManagerEndpointUrl"] || "https://management.azure.com/";
-  const azureSessionToken = await getAzureAccessToken(JSON.stringify(creds));
-
-  return await getAksKubeconfig(
-    azureSessionToken,
-    subscriptionId,
-    managementEndpointUrl,
-    resourceGroupName,
-    clusterName
-  );
-}
-
-export function getAksKubeconfig(
-  azureSessionToken: string,
-  subscriptionId: string,
-  managementEndpointUrl: string,
-  resourceGroupName: string,
-  clusterName: string
-): Promise<string> {
-  return new Promise<string>((resolve, reject) => {
-    const webRequest = new WebRequest();
-    webRequest.method = "GET";
-    webRequest.uri = `${managementEndpointUrl}/subscriptions/${subscriptionId}/resourceGroups/${resourceGroupName}/providers/Microsoft.ContainerService/managedClusters/${clusterName}/accessProfiles/clusterAdmin?api-version=2017-08-31`;
-    webRequest.headers = {
-      Authorization: "Bearer " + azureSessionToken,
-      "Content-Type": "application/json; charset=utf-8",
-    };
-
-    sendRequest(webRequest)
-      .then((response: WebResponse) => {
-        const accessProfile = response.body;
-        if (accessProfile?.properties?.kubeConfig) {
-          const kubeconfig = Buffer.from(
-            accessProfile.properties.kubeConfig,
-            "base64"
-          );
-          resolve(kubeconfig.toString());
-        } else {
-          reject(JSON.stringify(response.body));
-        }
-      })
-      .catch(reject);
-  });
+  core.debug("KUBECONFIG environment variable set");
 }
 
 run().catch(core.setFailed);
