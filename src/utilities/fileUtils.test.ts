0b3386a523d5d4cc8b25979c43c274a7ec50ae10
Vidya Reddy <vidyareddy@microsoft.com>
2022-07-08 12:07:53 -0700

    Traffic split annotations

---
+++ src/utilities/fileUtils.test.ts
@@ -0,0 +1,57 @@
+import {getFilesFromDirectories} from './fileUtils'
+
+import * as path from 'path'
+
+describe('File utils', () => {
+   it('detects files in nested directories and ignores non-manifest files and empty dirs', () => {
+      const testPath = path.join('test', 'unit', 'manifests')
+      const testSearch: string[] = getFilesFromDirectories([testPath])
+
+      const expectedManifests = [
+         'test/unit/manifests/manifest_test_dir/another_layer/deep-ingress.yaml',
+         'test/unit/manifests/manifest_test_dir/another_layer/deep-service.yaml',
+         'test/unit/manifests/manifest_test_dir/nested-test-service.yaml',
+         'test/unit/manifests/test-ingress.yml',
+         'test/unit/manifests/test-service.yml'
+      ]
+
+      expect(testSearch).toHaveLength(5)
+      expectedManifests.forEach((fileName) => {
+         expect(testSearch).toContain(fileName)
+      })
+   })
+
+   it('crashes when an invalid file is provided', () => {
+      const badPath = path.join('test', 'unit', 'manifests', 'nonexistent.yaml')
+      const goodPath = path.join(
+         'test',
+         'unit',
+         'manifests',
+         'manifest_test_dir'
+      )
+
+      expect(() => {
+         getFilesFromDirectories([badPath, goodPath])
+      }).toThrowError()
+   })
+
+   it("doesn't duplicate files when nested dir included", () => {
+      const outerPath = path.join('test', 'unit', 'manifests')
+      const fileAtOuter = path.join(
+         'test',
+         'unit',
+         'manifests',
+         'test-service.yml'
+      )
+      const innerPath = path.join(
+         'test',
+         'unit',
+         'manifests',
+         'manifest_test_dir'
+      )
+
+      expect(
+         getFilesFromDirectories([outerPath, fileAtOuter, innerPath])
+      ).toHaveLength(5)
+   })
+})
