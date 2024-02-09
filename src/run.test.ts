a13b6221d787621a6737c7151b2cb5ac7542767f
David Gamero <david340804@gmail.com>
2024-02-09 13:53:19 -0500

    format

--- src/run.test.ts
+++ src/run.test.ts
@@ -26,7 +26,7 @@ describe('Set context', () => {
             .mockImplementation((inputName, options) => {
                if (inputName == 'resource-group') return resourceGroup
                if (inputName == 'cluster-name') return ''
-	       return ''
+               return ''
             })
          await expect(run()).rejects.toThrow()
       },
